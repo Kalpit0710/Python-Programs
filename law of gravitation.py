8e8522553ac4f5186f3f3d50acefe4702371baa7
Kalpit Agarwal <112794724+Kalpit0710@users.noreply.github.com>
2022-09-04 11:31:38 +0530

    Add files via upload

---
+++ law of gravitation.py
@@ -0,0 +1,6 @@
+import math
+G=(6.67*10**(-11))
+m1=float(input("Enter Mass 1:"))
+m2=float(input("Enter Mass 2:"))
+r=float(input("Enter Distance Between them:"))
+print ((G*m1*m2)/r**2)
---
+++ multiply.py
@@ -0,0 +1,8 @@
+n1=int(input("Enter first number: :"))
+n2=int(input("enter second no.: "))
+prod=0
+count=n1
+while count>0:
+      count=count-1
+      prod=prod+n2
+print("the product of",n1,"and",n2,"is",prod)
