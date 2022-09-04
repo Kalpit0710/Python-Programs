4e6b4dcd343a7fc5ff3216ba11898ef7100ab456
Kalpit0710 <112794724+Kalpit0710@users.noreply.github.com>
2022-09-04 11:24:33 +0530

    Add files via upload

---
+++ day of week 1 sep.py
@@ -0,0 +1,17 @@
+a=int(input("Enter no of day"))
+if a==1:
+    print("Its Monday")
+elif a==2:
+    print("Its Tuesday")
+elif a==3:
+    print("Its Wednesday")
+elif a==4:
+    print("Its Thursday")
+elif a==5:
+    print("Its Friday")
+elif a==6:
+    print("Its Saturday")
+elif a==7:
+    print("Yaay!! Its Sunday")
+else:
+    print("!!! Wrong Input !!!")
